dbdac5b25d17352270d7801be87e3fe3fc2750ba
Marllon-Lima <marllonlima2004@gmail.com>
2025-04-29 23:23:09 -0300

    Ajuste no app.js

--- api_favoritos/src/app.js
+++ api_favoritos/src/app.js
@@ -2,13 +2,10 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 
-// Middleware para permitir o parsing de JSON no corpo das requisições
 app.use(express.json());
 
-// Usando as rotas
 app.use('/api', routes);
 
-// Porta para a API
 const port = 5002;
 app.listen(port, () => {
   console.log(`API Favoritos rodando na porta ${port}`);
